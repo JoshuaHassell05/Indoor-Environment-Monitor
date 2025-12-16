5e9bd1727a087545ee52814c0a6bdee09520bc7d
Joshua Hassell <157650123+JoshuaHassell05@users.noreply.github.com>
2025-12-16 03:16:17 +0000

    Define const char variables for wifi name, password, and the server url

--- firmware_esp32/src/main.cpp
+++ firmware_esp32/src/main.cpp
@@ -4,3 +4,7 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME680.h>
+const char* WIFI_SSID = "your_ssid";
+const char* WIFI_PASSWORD = "your_password";
+const char* SENSOR_POST_URL = "http://YOUR_SERVER_IP:5000/sensor";
+
