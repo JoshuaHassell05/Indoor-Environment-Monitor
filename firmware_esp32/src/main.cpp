1ccf76ba24e63f2a618de3c8f39a9f27006add81
Joshua Hassell <157650123+JoshuaHassell05@users.noreply.github.com>
2025-12-16 03:18:43 +0000

    Define BME680 chip and connectWiFi function that will attempt to connect and print out to serial the result

--- firmware_esp32/src/main.cpp
+++ firmware_esp32/src/main.cpp
@@ -7,4 +7,17 @@
 const char* WIFI_SSID = "your_ssid";
 const char* WIFI_PASSWORD = "your_password";
 const char* SENSOR_POST_URL = "http://YOUR_SERVER_IP:5000/sensor";
+Adafruit_BME680 bme; // I2C
+void connectWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWi-Fi connected");
+  Serial.print("ESP32 IP address: ");
+  Serial.println(WiFi.localIP());
+}
 
