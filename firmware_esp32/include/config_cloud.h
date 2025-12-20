aebc9a531bc04c2bf1594ca6d2e18e28ca3f3285
Joshua Hassell <157650123+JoshuaHassell05@users.noreply.github.com>
2025-12-20 23:55:26 +0000

    Update SENSOR_POST_URL to use a placeholder for API ID

--- firmware_esp32/include/config_cloud.h
+++ firmware_esp32/include/config_cloud.h
@@ -6,4 +6,4 @@
 #define WIFI_SSID "your_ssid"
 #define WIFI_PASSWORD "your_password"
 
-#define SENSOR_POST_URL "https://yc1b20bw9d.execute-api.us-east-1.amazonaws.com/Prod/sensor"
+#define SENSOR_POST_URL "https://YOUR_API_ID.execute-api.us-east-1.amazonaws.com/Prod/sensor"
