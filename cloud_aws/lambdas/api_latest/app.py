a32b9e8b291c84b40686a74e701c211c9160050e
Joshua Hassell <157650123+JoshuaHassell05@users.noreply.github.com>
2025-12-20 23:08:49 +0000

    Initialize DynamoDB resource and table in API

--- cloud_aws/lambdas/api_latest/app.py
+++ cloud_aws/lambdas/api_latest/app.py
@@ -3,3 +3,8 @@ import os
 from decimal import Decimal
 import boto3
 from boto3.dynamodb.conditions import Key
+
+# Initialize DynamoDB resource and table
+ddb = boto3.resource("dynamodb")
+table = ddb.Table(os.environ["TABLE_NAME"])
+
