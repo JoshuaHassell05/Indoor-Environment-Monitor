4fd4f2871e8cea0282c6d684e7de61d6f3901514
Joshua Hassell <157650123+JoshuaHassell05@users.noreply.github.com>
2025-12-20 23:13:24 +0000

    Initialize DynamoDB resource and table in API

--- cloud_aws/lambdas/api_readings/app.py
+++ cloud_aws/lambdas/api_readings/app.py
@@ -4,3 +4,8 @@ from datetime import datetime, timedelta, timezone
 from decimal import Decimal
 import boto3
 from boto3.dynamodb.conditions import Key
+
+# Initialize DynamoDB resource and table
+TABLE_NAME = os.environ.get("TABLE_NAME", "IndoorEnvReadings")
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(TABLE_NAME)
