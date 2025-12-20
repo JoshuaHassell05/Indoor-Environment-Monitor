97e0ddd817b3ece8f5e1d25700fca510f77dbd74
Joshua Hassell <157650123+JoshuaHassell05@users.noreply.github.com>
2025-12-20 23:43:35 +0000

    Remove cloud device configuration from header file

--- firmware_esp32/include/config_cloud.h
+++
@@ -1,5 +0,0 @@
-
-#pragma once
-
-#define DEVICE_ID "device-1"
-#define POST_INTERVAL_MS 15000
---
+++ firmware_esp32/include/config_local.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "config_common.h"
+
+#define USE_HTTPS 0
+
+#define WIFI_SSID "your_ssid"
+#define WIFI_PASSWORD "your_password"
+
+#define SENSOR_POST_URL "http://YOUR_SERVER_IP:5000/sensor"
